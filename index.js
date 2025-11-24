b92e68e48897eff76a176765c4e65760ab5a7d8b
Maruf131 <mdsheikhmaruf131@gmail.com>
2025-11-24 19:55:52 +0600

    added application post API

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port =process.env.PORT || 5000;
 
-// middleWare
+// middle Ware
 app.use(cors())
 app.use(express.json())
 
@@ -26,6 +26,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollections = client.db('careerCode').collection('jobs');
+    const applicationCollection = client.db('careerCode').collection('applications')
+    //Find jobs API
+    app.get('/jobs', async(req, res) =>{
+        const cursor = jobsCollections.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+    // findOne jobs API
+    app.get('/jobs/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)};
+        const result = await jobsCollections.findOne(query);
+        res.send(result);
+    })
+    //application post API
+    app.post('/application', async(req, res)=>{
+        const application = req.body;
+        const result = await applicationCollection.insertOne(application);
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
