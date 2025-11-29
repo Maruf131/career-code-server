3a0c7b4a9fcc788f836d6234e5bf4aab9284c1fd
Maruf131 <mdsheikhmaruf131@gmail.com>
2025-11-29 14:05:11 +0600

    added new commit

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port =process.env.PORT || 5000;
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5001;
 
 // middle Ware
-app.use(cors())
-app.use(express.json())
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uw5xzgl.mongodb.net/?appName=Cluster0`;
 
@@ -19,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,43 +25,72 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const jobsCollections = client.db('careerCode').collection('jobs');
-    const applicationCollection = client.db('careerCode').collection('applications')
+    const jobsCollections = client.db("careerCode").collection("jobs");
+    const applicationCollection = client
+      .db("careerCode")
+      .collection("applications");
     //Find jobs API
-    app.get('/jobs', async(req, res) =>{
-        const cursor = jobsCollections.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-    // findOne jobs API
-    app.get('/jobs/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)};
-        const result = await jobsCollections.findOne(query);
-        res.send(result);
-    })
-    //application post API
-    app.post('/application', async(req, res)=>{
-        const application = req.body;
-        const result = await applicationCollection.insertOne(application);
-        res.send(result)
-    })
+    app.get("/jobs", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
 
+      const cursor = jobsCollections.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // findOne jobs API
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollections.findOne(query);
+      res.send(result);
+    });
+    // insert jobs
+    app.post("/jobs", async (req, res) => {
+      const newJobs = req.body;
+      const result = await jobsCollections.insertOne(newJobs);
+      res.send(result);
+    });
+
+    app.get("/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        applicant: email,
+      };
+      const result = await applicationCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/applications/job/job_id", async (req, res) => {
+      const job_id = req.params.job_id;
+      const query = { jobId: job_id };
+      const result = await applicationCollection.find(query).toArray();
+      res.send(result)
+    });
+    //application post API
+    app.post("/application", async (req, res) => {
+      const application = req.body;
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Career code is running')
-})
+app.get("/", (req, res) => {
+  res.send("Career code is running");
+});
 
 app.listen(port, () => {
-  console.log(`Career code is running on port ${port}`)
-})
+  console.log(`Career code is running on port ${port}`);
+});
